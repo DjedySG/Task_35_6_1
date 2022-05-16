258fd5164b6a8fa2a30af64df641b2e6732b8fbb
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-05-16 19:58:55 +0400

    Add files via upload

---
+++ Task_35_6_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+class Row
+{
+public:
+    Row(std::initializer_list<int> numbers)
+    {
+        m_Row = numbers;
+    }
+    std::vector<int> m_Row;
+};
+
+int main()
+{
+    Row myRow = { 1,2,3,4,5 };
+    for (auto &i : myRow.m_Row )
+    {
+        std::cout << i;
+    }
+       
+}
+
